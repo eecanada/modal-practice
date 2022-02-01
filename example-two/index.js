2132d6cf540d6f9759a6a9d7e3a0a86728269392
Eder Canada <canada.eder@gmail.com>
2022-02-01 11:52:40 -0800

    open up modal

--- example-two/index.js
+++ example-two/index.js
@@ -1,2 +1,12 @@
-'use strict'
+'use strict';
 
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModalButton = document.querySelector('.close-modal');
+const openModalButtons = document.querySelectorAll('.show-modal');
+
+openModalButtons.forEach((openModalButton) =>
+  openModalButton.addEventListener('click', () =>
+    modal.classList.remove('hidden')
+  )
+);
